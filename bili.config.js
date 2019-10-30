1346af2a5842a0f14274c7de00a67b73fb4c56ee
Marinko Peso <marinko.peso@gmail.com>
2019-10-30 10:15:56 +0100

    add initial build process with bili 👨🏻‍🚒

---
+++ bili.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  banner: true,
+  input: 'index.js',
+  output: {
+    extractCSS: false
+  },
+  plugins: {
+    vue: {
+      css: true
+    }
+  }
+};
---
+++ index.js
@@ -0,0 +1,7 @@
+import PureModal from './src/PureModal.vue';
+
+export default {
+  install(Vue) {
+    Vue.component('pure-modal', PureModal);
+  }
+};
